9761e5a369be9d2b8d808880fb69d998f873fea7
Vysakh P J <vysakhj@spanidea.com>
2020-02-18 10:05:05 +0530

    main.c file created

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include"pool.h"
+
 void address_store(void *array_store[], void *address)
 {
     int i = 0;
@@ -7,6 +9,7 @@ void address_store(void *array_store[], void *address)
         i++;
     array_store[i] = address;
 }
+
 void *return_address(void *array_address[])
 {
     int i = 0, flag;
@@ -26,13 +29,19 @@ void *return_address(void *array_address[])
         return 0;
     }
 }
+
 int main()
 {
     void *head;
     void *address;
     void *array[100] = {NULL};
-    pool_t pool;
-    init(&pool, 50, 5);
+    pool_t pool = {NULL};
+    node_t* initial_pool;
+    pool.block_size = 5000;
+    pool.block_count = 5; 
+    init(&pool, pool.block_size, pool.block_count);
+    pool.blocks = pool.head;
+    initial_pool = pool.head;
     head = get(&pool);
     address_store(array, head);
     address = return_address(array);
@@ -46,6 +55,16 @@ int main()
     address_store(array, head);
     head = get(&pool);
     address_store(array, head);
+    head = get(&pool);
+    address_store(array, head);
+    head = get(&pool);
+    address_store(array, head);
+    head = get(&pool);
+    address_store(array, head);
+    head = get(&pool);
+    address_store(array, head);
+    head = get(&pool);
+    address_store(array, head);
     put(&pool,address);
         address = return_address(array);
     put(&pool,address);
@@ -74,6 +93,20 @@ int main()
         address = return_address(array);
     put(&pool,address);
         address = return_address(array);
-    // de_init(&pool);
+         put(&pool,address);
+        address = return_address(array);
+    put(&pool,address);
+        address = return_address(array);
+         put(&pool,address);
+        address = return_address(array);
+    put(&pool,address);
+        address = return_address(array);
+    put(&pool,address);
+        address = return_address(array);
+         put(&pool,address);
+        address = return_address(array);
+    put(&pool,address);
+        address = return_address(array);
+    de_init(&pool);
     return 0;
 }
