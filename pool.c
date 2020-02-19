ab9ef4ea25e9a3cf058ce002046b2e08c6f78e7f
Vysakh P J <vysakhj@spanidea.com>
2020-02-19 18:40:20 +0530

    freelist and usedlist created

--- pool.c
+++ pool.c
@@ -1,79 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "pool.h"
 
-void init(pool_t *pool, int size, int n)
+static int block_alloc(pool_t* pool)
 {
-    node_t *temp_node = pool->head;
-    node_t *temp = NULL;
-    pool->head = (node_t *)malloc((n)*size);
-    printf("temp_head %p\n",pool->head);
+    node_t *temp = NULL; 
+    size_t memcnt = sizeof(node_t)+(pool->block_size * pool->block_count);
+    node_t *temp_block = (node_t *)malloc(memcnt); 
+    if(!temp_block)
+        return (-1);
+    memset(temp_block, 0, memcnt);
+    temp_block->next = pool->blocks;
+    pool->blocks = temp_block;
+    pool->head = pool->blocks + 1;
+    // if(temp_block!= NULL)
+    //     temp_block->next = pool->head;
+    pool->free_address = pool->head;
     temp = pool->head;
-    // if(temp_node!=NULL)
-    // {
-    //     while(temp_node->next != NULL)
-    //         temp_node = temp_node+size;
-    //     temp_node->next = temp;
-    // }
-    pool->depth = 5;
-    for (int i = 0; i < n - 1; i++)
+
+    pool->depth += pool->block_count;
+    for (int i = 0; i < pool->block_count ; i++)
     {
-        temp->next = temp + size;
+        printf("temp %d\n",temp);
+        temp->next = (void*)temp + pool->block_size;
         temp = temp->next;
-        printf("temp_head %p\n",temp);
     }
+    printf("\n\n");
+    return 0;
+}
+
+int init(pool_t *pool, int size, int n)
+{
+    memset(pool, 0, sizeof(pool_t));
+    pool->block_size = size;
+    pool->block_count = n;
+    block_alloc(pool);
+    if(pool->head)
+        return(0);
+    else
+        return(-1);
+    
 }
 
 void *get(pool_t *pool)
 {
-    if (pool->depth == 0)
-    {
-        blockalloc(pool,pool->block_size,pool->block_count);    
-    }
     node_t *temp = pool->head;
-    printf("pool_head %p\n",temp);
-    printf("pool_next %p\n",temp->next);
-    pool->head = temp->next;
+    if(pool->depth == 0)
+    {
+        block_alloc(pool);
+        temp = pool->head;
+    }
+    printf("borrowing %d\n",pool->free_address);
+    pool->free_address = pool->free_address->next;
+    //pool->head = temp->next;
+    pool->used_address = pool->head;
     pool->depth--;
-    printf("pool depth %d\n",pool->depth);
-    return temp;
+    return 0;
 }
 
-void put(pool_t *pool, void *address)
+void put(pool_t *pool)
 {
-    int i = 0;
-    node_t *temp = address;
-    if (temp != NULL)
+    // node_t *temp = address;
+    if (pool->used_address)
     {
-        temp->next = pool->head;
-        pool->head = temp;
+        pool->head = pool->head->next;
+        printf("returning %d\n",pool->used_address);
+        pool->used_address->next = pool->free_address->next;
+        pool->free_address->next = pool->used_address;
+        pool->used_address = pool->head;
+        // temp->next = pool->head;
+        // pool->head = temp;
+        // printf("returning %d\n",pool->used);
         pool->depth++;
     }
 }
 
 void de_init(pool_t *pool)
 {
-    if (pool && pool->blocks)
+    node_t* temp_node = pool->blocks;
+    while (pool && pool->blocks->next)
     {
         free(pool->blocks);
+        if(temp_node->next != NULL) 
+            pool->blocks = pool->blocks->next;                
     }
-    pool->blocks = NULL;
-    pool->head = NULL;
-    pool->depth = 0;
-}
-int blockalloc(pool_t* pool,int size,int n)
-{
-    printf("hi\n");
-    node_t *temp_node = pool->head;
-    node_t *temp = NULL;
-    pool->head = (node_t *)malloc((n)*size);
-    if(!pool->head)
-        return (-1);
-    for (int i = 0; i < n - 1; i++)
-    {
-        printf("temp_head %p\n",temp);
-        temp->next = temp + size;
-        temp = temp->next;
-    }
-    return (0);
 }
